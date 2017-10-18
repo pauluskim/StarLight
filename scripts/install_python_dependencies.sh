4c69c6314e9fe3b8c19e25426af451adc3e40ab7
pauluskim <jkkim9103@gmail.com>
2017-10-18 22:28:19 +0900

    try to use codedeploy

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+sudo apt-get install python-mysqldb
 pip install --upgrade pip
 pip install -r /home/ubuntu/webapps/starlight/requirements.txt
---
+++ scripts/runserver.sh
@@ -0,0 +1,2 @@
+cd starlight
+python manage.py runserver 0.0.0.0:80
