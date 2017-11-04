69466f6fa90ee53078f0372af152da0df8b730c2
pauluskim <jkkim9103@gmail.com>
2017-11-04 14:51:16 +0900

    recursive bug fixes: step back

--- starlight/crawler/views.py
+++ starlight/crawler/views.py
@@ -178,9 +178,10 @@ def check_influencer(request):
         else:
             print "Caught Influencer."
             crawler_domain = ip_list[crawler_index]
-            if recursive_step == '2': continue 
+            if recursive_step == '3': continue 
             recursive_step = str(int(recursive_step)+1)
             requests.get(crawler_domain+"crawl/user_follow?next_function={}&target_user_pk={}&recursive_step={}".format("check_influencer", str(json_response["target_user_pk"]), recursive_step))
+            recursive_step = str(int(recursive_step)-1)
             crawler_index = (crawler_index + 1) % num_crawler
 
     return JsonResponse({'success': True})
@@ -295,7 +296,7 @@ def extract_hash_tags(s):
 def user_by_name(request):
     username = request.GET.get("username", "")
     recursive = request.GET.get("recursive", 'False')
-    if User.objects.count() > 10000: return JsonResponse({'success': False, 'target_user_pk':"FULL"})
+    if User.objects.count() > 40000: return JsonResponse({'success': False, 'target_user_pk':"FULL"})
     if User.objects.filter(username = username).exists():
         if recursive == 'False':
             return JsonResponse({'success': True, 'target_user_pk':User.objects.get(username = username).user_pk})
